fb9557469c88f6b886408ff3286289547c90bed5
yTrayanov <ecokatana1925@abv.bg>
2021-09-10 15:40:07 +0300

    Delete server directory

--- server/config/emailjs.js
+++
@@ -1,5 +0,0 @@
-const emailjs = require('emailjs-com');
-
-module.exports =  (() => {
-    emailjs.init('user_CBFjjlI7LoTbxcKH4fLE6');
-});
--- server/index.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-const config = require('./config/config')[env];
-require('./config/database')(config);
-const app = require('express')();
-require('./config/express')(app);
-require('./config/emailjs')();
-
-app.listen(config.port, console.log("Listening on port " + config.port + "..."));
